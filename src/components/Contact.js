6243e4c3ad8ec0ee8bef6d588943680928f0bc16
austinald1 <81729807+austinald1@users.noreply.github.com>
2021-09-23 21:38:37 -0700

    added text

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,8 +3,9 @@ import React from "react";
 function Contact(){
     return (
         <div>
-            <h1>Contact</h1>
-            <div>contact content goes in here</div>
+            <h1>Robert Austin Alderete</h1>
+            <div>https://www.linkedin.com/in/austin-alderete</div>
+            <div>https://github.com/austinald1</div>
         </div>
     );
 }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,7 +4,11 @@ function Portfolio(){
     return (
         <div>
             <h1>Portfolio</h1>
-            <div>This is my Portfolio</div>
+                <div>2005 - 2015 Residenial Construction Independent </div>
+                <div>2015-Current Commercial Construction</div>
+                <div>S.C. Grade 2018-2020</div>
+                <div>Riggs Companies since May of 2020 to current</div>
+                <div>I am also a full stack developer enrolled in the University of Arizona Coding Bootcamp</div>
         </div>
     );
 }
